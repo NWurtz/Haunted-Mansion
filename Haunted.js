afc1865e2e95e8294d758e4a204ea377f1954451
MichaelParisi <michael.parisi1@marist.edu>
2013-11-12 13:42:28 -0500

    Update Haunted.js

--- Haunted.js
+++ Haunted.js
@@ -1,4 +1,32 @@
+
 function startGame()
 {
-return document.getElementById('outputDiv').innerHTML ='Background info'
+
+	return document.getElementById('outputDiv').innerHTML ='Background info'
+
 }
+
+/*
+// Show and hide
+	var btn;
+
+	btn.style.visibility = 'hidden';
+	btn.style.visibility = 'visible';
+
+	btn.style.display = 'none';
+	btn.style.display = 'inline'; // for a div or block element, set it to 'block'
+
+
+*/
+
+/*
+
+//Make new tags
+
+	var div = document.getElementById('divId');
+
+	div.innerHTML = "<button>Button Text</button><button>Other button</button>";
+
+	//////////////////////////////
+
+*/
